8c6de410e9406f2d67e106954e202269034808db
Martin Krockert <martin.krockert@gmail.com>
2016-05-21 15:24:21 +0200

    update vom CA-System

--- GraphSystem/bin/app.js
+++ GraphSystem/bin/app.js
@@ -1,127 +1,137 @@
 angular.module('app' , [])
 .controller('HomeController', function ($attrs, $interval, $scope) {
+  $scope.initDensity = parseInt(500);
+  $scope.initDistribution = parseInt(10);
+  $scope.dividePercent = parseInt(70);
+  $scope.flipPercent = parseInt(30);
+  $scope.stepcounter = 0;
+  $scope.algorithm = {
+    voronoi : true,
+    delaunay : false,
+    pointers : true,
+  }
+  var stop;
+  // Canvas Context
+  var canvas = document.getElementById("c"),
+      context = canvas.getContext("2d"),
+      width = canvas.width = 600,
+      height = canvas.height = 600;
+  // Voronoi Parameters
+  var particles = new Array($scope.initDensity);
+  var voronoi = d3_voronoi.voronoi()
+      .extent([[-1, -1], [width + 1, height + 1]]);
+  var VoronoiGeo;
+  $scope.generateMap =  function() {
+      // initialize the map - Create a Random Dot Matrix
+      for (var i = 0; i < parseInt($scope.initDensity); ++i) {
+          var cellType = 'healthy';
+          if (Math.random() < (parseInt($scope.initDistribution) / 100)) {
+            cellType = 'infected';
+          }
+          particles[i] = {0: Math.random() * width,
+                          1: Math.random() * height,
+                          vx: 0,
+                          vy: 0,
+                          cellType: cellType};
+          }
+      $scope.reDrawGraph();
+      // start voronoi teslation and Create its TreeStructure
 
-      $scope.algorithm = {
-        voronoi : true,
-        delaunay : false
-      }
-      var canv = null;
-      var colors = [];
-      var map = new surface(600, 600);
-      var stop;
-      $scope.initDensity = parseInt(500);
-      $scope.dividePercent = parseInt(70);
-      $scope.flipPercent = parseInt(30);
-      $scope.stepcounter = 0;
+  }
 
 
-      $scope.generateMap =  function() {
-          // initial node to make nodes smaller (nodesize is relativ)
+  
 
-          canv = document.getElementById("c");
-  				c = canv.getContext("2d");
-  				w = canv.width = 600;
-  				h = canv.height = 600;
-          points = [];
-          map.cells = [];
-          map.edges = [];
-          v = new Voronoi();
+  $scope.reDrawGraph = function() {
+    var ds_voronoi = voronoi(particles);
+    var topology = computeTopology(ds_voronoi);
+    VoronoiGeo = topojson.mesh(topology, topology.objects.voronoi, function(a, b) { return a !== b; });;
+    context.clearRect(0, 0, width, height);
 
-  				for(i=0; i<parseInt($scope.initDensity); i++)
-  				{
-            p = new Point(roundToTwo(Math.random()*w), roundToTwo(Math.random()*h));
-  					points.push(p);
-            map.cells.push(new cell(p.x, p.y, 'free'));
-  					// colors.push('#BDBDBD');
-  				}
-  				redraw();
-      }
+    // VORONOI
+    if ($scope.algorithm.voronoi) {
+      // Draw whole Voronoi
+      context.beginPath();
+      renderMultiLineString(context, VoronoiGeo);
+      context.strokeStyle = "rgba(0,0,0,0.4)";
+      context.lineWidth = 0.5;
+      context.stroke();
 
-      $scope.reDrawGraph =  function() {
-        redraw();
-      }
 
-      $scope.generateGraph = function() {
-        var end = edges.length;
-        for(i=0; i<end; i++)
-        {
-          var p1 = new Point(roundToTwo(edges[i].left.x), roundToTwo(edges[i].left.y));
-          var p2 = new Point(roundToTwo(edges[i].right.x), roundToTwo(edges[i].right.y));
-          map.addEdgeIfNotExist(p1,p2);
+      // Colorize Infected Cells.
+      context.beginPath();
+      renderMultiPolygon(context, topojson.merge(topology, topology.objects.voronoi.geometries.filter(
+        function(d) { return d.data.cellType === 'infected'; })));
+      context.fillStyle = "rgba(255,0,0,0.1)";
+      context.fill();
+      context.lineWidth = 1.5;
+      context.lineJoin = "round";
+      context.strokeStyle = "rgba(255,0,0,1)";
+      context.stroke();
+    }
+
+    // POINTS
+    if ($scope.algorithm.pointers) {
+      particles.forEach(function(p, i) {
+        context.beginPath();
+        context.arc(p[0], p[1], 2.5, 0, 2 * Math.PI);
+        if (p.cellType == 'healthy') {
+          context.fillStyle = "rgba(0,0,0,0.6)";
+        } else {
+          context.fillStyle = "rgba(255,0,0,1)";
         }
-        map.logEdges();
-      }
+        context.fill();
+      });
+    }
+
+    // DELAUNAY
+    if ($scope.algorithm.delaunay) {
+      context.beginPath();
+      var delaunay = d3.geom.delaunay(particles);
+      context.setLineDash([5, 15]);
+      renderDelaunay(context, delaunay);
+      context.strokeStyle = "rgba(0,0,0,1)";
+      context.lineWidth = 0.5;
+      context.stroke();
+      context.setLineDash([1]);
+    }
+
+    // calc Area all given Poligons and return its size
+    // not sure if correct.
+    // calcPolygonArea(topojson.mesh(topology, topology.objects.voronoi));
+    // console.log(area);
+
+}
 
 
+// Drawing Functions.
+  function renderMultiLineString(context, line) {
+    line.coordinates.forEach(function(line) {
+      line.forEach(function(point, i) {
+        if (i) context.lineTo(point[0], point[1]);
+        else context.moveTo(point[0], point[1]);
+      });
+    });
+  }
+
+  function renderMultiPolygon(context, polygon) {
+    polygon.coordinates.forEach(function(polygon) {
+      polygon.forEach(function(ring) {
+        ring.forEach(function(point, i) {
+          if (i) context.lineTo(point[0], point[1]);
+          else context.moveTo(point[0], point[1]);
+        });
+      });
+    });
+  }
+
+  function renderDelaunay(contect, line) {
+    for (var i = 0; i < line.length; i++) {
+      context.moveTo(line[i][0][0],line[i][0][1]);
+      context.lineTo(line[i][1][0],line[i][1][1]);
+      context.lineTo(line[i][2][0],line[i][2][1]);
+    }
+  }
 
 
-      function redraw()
-			{
-				c.fillStyle = "#ffffff";
-				c.fillRect (0, 0, w, h);
-
-				v.Compute(points, w, h);
-				edges = v.GetEdges();
-				cells = v.GetCells();
-
-        // colorizing
-        /*
-        for(var i=0; i<cells.length; i++)
-				{
-					var p = cells[i].vertices;
-					if(p.length == 0) continue;
-					if(p.length == 4)
-					{
-						console.log(cells[i].vs);
-						console.log(p);
-					}
-					c.fillStyle = colors[i];
-					c.beginPath();
-					c.moveTo(p[0].x, p[0].y);
-					for(var j=1; j<p.length; j++) c.lineTo(p[j].x, p[j].y);
-					c.closePath();
-					c.fill();
-				}
-        */
-				if($scope.algorithm.delaunay)
-				{
-					c.lineWidth = 1;
-					c.strokeStyle = "#888888";
-					for(i=0; i<edges.length; i++)
-					{
-						var e = edges[i];
-						c.beginPath();
-						c.moveTo(e.left.x, e.left.y);
-						c.lineTo(e.right.x, e.right.y);
-						c.closePath();
-						c.stroke();
-					}
-				}
-
-				if($scope.algorithm.voronoi)
-				{
-					c.lineWidth = 2;
-					c.strokeStyle = "#000";
-					for(i=0; i<edges.length; i++)
-					{
-						var e = edges[i];
-						c.beginPath();
-						c.moveTo(e.start.x, e.start.y);
-						c.lineTo(e.end.x, e.end.y);
-						c.closePath();
-						c.stroke();
-					}
-				}
-
-				c.fillStyle = "rgb(255,0,0)";
-				for(i=0; i<points.length; i++)
-				{
-					var p = points[i];
-					c.beginPath();
-					c.arc(p.x, p.y, 3, 0, Math.PI*2, true);
-					c.closePath();
-					c.fill();
-				}
-			}
-      // Transform the Triangulation into Network based Graph.
  });
--- GraphSystem/bin/helper.js
+++ GraphSystem/bin/helper.js
@@ -1,3 +1,92 @@
 function roundToTwo(num) {
     return +(Math.round(num + "e+2")  + "e-2");
 }
+function computeTopology(diagram) {
+  var cells = diagram.cells,
+      x0 = diagram.extent[0][0],
+      y0 = diagram.extent[0][1],
+      x1 = diagram.extent[1][0],
+      y1 = diagram.extent[1][1],
+      arcs = [],
+      arcIndex = -1,
+      arcIndexByEdge = {};
+
+  return {
+    objects: {
+      voronoi: {
+        type: "GeometryCollection",
+        geometries: cells.map(function(cell) {
+          var cell,
+              site = cell.site,
+              halfedges = cell.halfedges,
+              cellArcs = [],
+              clipArc;
+
+          // If this cell has edges…
+          if (halfedges.length) {
+            halfedges.forEach(function(halfedge) {
+              var edge = diagram.cellEdges[halfedge];
+              if (edge.right) {
+                var l = edge.left.index,
+                    r = edge.right.index,
+                    k = l + "," + r,
+                    i = arcIndexByEdge[k];
+                if (i == null) arcs[i = arcIndexByEdge[k] = ++arcIndex] = edge;
+                cellArcs.push(site === edge.left ? i : ~i);
+                clipArc = null;
+              } else if (clipArc) { // Coalesce border edges.
+                if (edge.left) edge = edge.slice(); // Copy-on-write.
+                clipArc.push(edge[1]);
+              } else {
+                arcs[++arcIndex] = clipArc = edge;
+                cellArcs.push(arcIndex);
+              }
+            });
+
+            // Ensure the last point in the polygon is identical to the first point.
+            var firstArcIndex = cellArcs[0],
+                lastArcIndex = cellArcs[cellArcs.length - 1],
+                firstArc = arcs[firstArcIndex < 0 ? ~firstArcIndex : firstArcIndex],
+                lastArc = arcs[lastArcIndex < 0 ? ~lastArcIndex : lastArcIndex];
+            lastArc[lastArcIndex < 0 ? 0 : lastArc.length - 1] = firstArc[firstArcIndex < 0 ? firstArc.length - 1 : 0].slice();
+          }
+
+          // Otherwise, assume that the polygon subsumes the extent.
+          else if (site[0] >= x0 && site[0] <= x1 && site[1] >= y0 && site[1] <= y1) {
+            arcs[++arcIndex] = [[x0, y1], [x1, y1], [x1, y0], [x0, y0], [x0, y1]];
+            cellArcs.push(arcIndex);
+          }
+
+          return {
+            type: "Polygon",
+            data: site.data,
+            arcs: [cellArcs]
+          };
+        })
+      }
+    },
+    arcs: arcs
+  };
+}
+
+function calcPolygonArea(v) {
+    var total = 0;
+    for (var k = 0; k < v.coordinates.length; k++) {
+      var cellTotal = 0;
+      var vertices = v.coordinates[k];
+      for (var i = 0, l = vertices.length; i < l; i++) {
+        var addX = vertices[i][0];
+        var addY = vertices[i == vertices.length - 1 ? 0 : i + 1][1];
+        var subX = vertices[i == vertices.length - 1 ? 0 : i + 1][0];
+        var subY = vertices[i][1];
+
+        cellTotal += (addX * addY * 0.5);
+        cellTotal -= (subX * subY * 0.5);
+      }
+      console.log('cell total: ' + Math.abs(cellTotal));
+      total = total + Math.abs(cellTotal);
+    }
+    console.log('Total: ' + Math.abs(total));
+
+    // return ;
+}
